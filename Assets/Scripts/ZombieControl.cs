cd0f9bf2906b7517f42e8193aa0a36abb77a0f8f
idol83 <igorn2148@gmail.com>
2023-01-15 18:14:31 +0300

    Do zombie atack 1

--- Assets/Scripts/ZombieControl.cs
+++ Assets/Scripts/ZombieControl.cs
@@ -1,14 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Opsive.UltimateCharacterController.Character;
-using Opsive.UltimateCharacterController.Character.Abilities;
 using Opsive.UltimateCharacterController.Character.Abilities.AI;
 using Opsive.UltimateCharacterController.Character.Abilities.Items;
+using Unity.Mathematics;
 
 public class ZombieControl : MonoBehaviour
 {
     [SerializeField] private Transform _zombieTarget;
+    [SerializeField] private float _attackDistance = 1.1f;
     private UltimateCharacterLocomotion _characterLocomotion;
     private NavMeshAgentMovement _navMeshAgentMovement;
     private Use _useAbility;
@@ -20,10 +19,12 @@ public class ZombieControl : MonoBehaviour
     }
     private void Update()
     {
-        _navMeshAgentMovement.SetDestination(_zombieTarget.position);
-        if (Vector3.Distance(transform.position, _zombieTarget.position) < 1.1f)
-        {
+        if (Vector3.Distance(transform.position, _zombieTarget.position) < _attackDistance)
             _characterLocomotion.TryStartAbility(_useAbility);
+        else
+        {
+            _navMeshAgentMovement.SetDestination(_zombieTarget.position);
+            _characterLocomotion.TryStopAbility(_useAbility);
         }
     }
 }
